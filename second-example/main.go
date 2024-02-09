1e3df3d56b570cc1dda7c5aad167dc4d47ddd7ef
Tornike Gogberashvili <toko.gogberaa@gmail.com>
2024-02-09 13:02:19 +0600

    Create bank balance example

--- second-example/main.go
+++ second-example/main.go
@@ -5,43 +5,50 @@ import (
 	"sync"
 )
 
-var msg string
 var wg sync.WaitGroup
 
-//func updateMessage(s string, m *sync.Mutex) {
-//	defer wg.Done()
-//
-//	m.Lock()
-//	msg = s
-//	m.Unlock()
-//}
-//
-//func main() {
-//	msg = "Hello, world!"
-//
-//	var mutex sync.Mutex
-//
-//	wg.Add(2)
-//	go updateMessage("Hello, universe!", &mutex)
-//	go updateMessage("Hello, cosmos!", &mutex)
-//	wg.Wait()
-//
-//	fmt.Println(msg)
-//}
-
-func updateMessage(s string) {
-	defer wg.Done()
-
-	msg = s
+type Income struct {
+	Source string
+	Amount int
 }
 
 func main() {
-	msg = "Hello, world!"
+	// variable for bank balance
+	var bankBalance int
+	var balance sync.Mutex
 
-	wg.Add(2)
-	go updateMessage("Hello, universe!")
-	go updateMessage("Hello, cosmos!")
+	// print out starting value
+	fmt.Printf("Initial account balance: $%d.00", bankBalance)
+	fmt.Println()
+
+	// define weekly revenue
+	incomes := []Income{
+		{"Main job", 500},
+		{"Gifts", 10},
+		{"Part time job", 50},
+		{"Investments", 100},
+	}
+
+	wg.Add(len(incomes))
+
+	// loop through 52 weeks and print out how much is made; keep a running total
+	for i, income := range incomes {
+		go func(i int, income Income) {
+			defer wg.Done()
+			for week := 1; week <= 52; week++ {
+				balance.Lock()
+				temp := bankBalance
+				temp += income.Amount
+				bankBalance = temp
+				balance.Unlock()
+
+				fmt.Printf("On week %d, you earned $%d.00 from %s\n", week, income.Amount, income.Source)
+			}
+		}(i, income)
+	}
 	wg.Wait()
 
-	fmt.Println(msg)
+	// print out final balance
+	fmt.Printf("Final bank balance: $%d.00", bankBalance)
+	fmt.Println()
 }
--- second-example/main_test.go
+++ second-example/main_test.go
@@ -1,17 +1,1 @@
 package main
-
-import "testing"
-
-func Test_updateMessage(t *testing.T) {
-	msg = "Hello, world!"
-
-	wg.Add(2)
-	go updateMessage("x")
-	go updateMessage("Goodbye, cool world!")
-
-	wg.Wait()
-
-	if msg != "Goodbye, cool world!" {
-		t.Errorf("Incorrect value in msg")
-	}
-}
