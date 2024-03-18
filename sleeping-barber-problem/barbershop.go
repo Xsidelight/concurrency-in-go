eabb595ed2027f6264946a18c4763370ee757124
Tornike Gogberashvili <xside@Tornikes-MacBook-Pro.local>
2024-03-19 00:40:44 +0500

    Implement addBarber function

--- sleeping-barber-problem/barbershop.go
+++ sleeping-barber-problem/barbershop.go
@@ -1,6 +1,10 @@
 package main
 
-import "time"
+import (
+	"time"
+
+	"github.com/fatih/color"
+)
 
 type Barbershop struct {
 	ShopCapacity    int
@@ -9,4 +13,23 @@ type Barbershop struct {
 	BarberDoneChan  chan bool
 	ClientsChan     chan string
 	Open            bool
+}
+
+func (shop *Barbershop) addBarber(barber string) {
+	shop.NumberOfBarbers++
+
+	go func() {
+
+	}()
+}
+
+func (shop *Barbershop) cutHair(barber, client string) {
+	color.Green("%s is cutting %s's hair.", barber, client)
+	time.Sleep(shop.CutDuration)
+	color.Green("%s is finished cutting %s", barber, client)
+}
+
+func (shop *Barbershop) sendBarberHome(barber string) {
+	color.Cyan("%s is going home.", barber)
+	shop.BarberDoneChan <- true
 }
