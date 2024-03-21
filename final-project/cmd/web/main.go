f232d2252e3acb4198512a6c13f0e94f09285d02
Tornike Gogberashvili <toko.gogberaa@gmail.com>
2024-03-21 16:52:34 +0500

    Add docker compose file add necessary go libraries

---
+++ final-project/cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+const webPort = "80"
+
+func main() {
+	// connect to DB
+
+	// create sessions
+
+	// create channels
+
+	// create waitgroup
+
+	// set up the application config
+
+	// set up mail
+
+	// listen for web communication
+
+}
