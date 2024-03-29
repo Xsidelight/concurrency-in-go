afb9f1af2e7f8313637cca62fbb7568cc20206da
Tornike Gogberashvili <toko.gogberaa@gmail.com>
2024-03-29 11:08:21 +0500

    Add send email on wrong password

--- final-project/cmd/web/handlers.go
+++ final-project/cmd/web/handlers.go
@@ -39,6 +39,15 @@ func (app *Config) PostLoginPage(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if !validPassword {
+		// in production you may want to track the number of logins
+		msg := Message{
+			To:      email,
+			Subject: "Failed login attempt",
+			Data:    "Invalid login attempt!",
+		}
+
+		app.sendEmail(msg)
+
 		app.Session.Put(r.Context(), "error", "Invalid credentials.")
 		http.Redirect(w, r, "/login", http.StatusSeeOther)
 		return
